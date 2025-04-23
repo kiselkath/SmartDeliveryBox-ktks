73cca721c7b56e58b885193ba743ad1f30433960
katekiselevich <kiselkath@gmail.com>
2025-04-23 09:43:16 +0200

    Create a Generic Class

---
+++ src/src/SmartBox.java
@@ -0,0 +1,12 @@
+public class SmartBox<T> {
+
+    private T item;
+
+    public void addItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return this.item;
+    }
+}

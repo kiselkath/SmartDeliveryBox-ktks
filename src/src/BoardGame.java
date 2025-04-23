7528a5942f054cfba7e02137b8eb14443d8c3410
katekiselevich <kiselkath@gmail.com>
2025-04-23 09:53:07 +0200

    Create at least two item classes - Book.java, BoardGame.java

---
+++ src/src/BoardGame.java
@@ -0,0 +1,11 @@
+public class BoardGame {
+    private String gameTitle;
+
+    public BoardGame(String gameTitle){
+        this.gameTitle = gameTitle;
+    }
+
+    public String getGameTitle(){
+        return gameTitle;
+    }
+}
---
+++ src/src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String bookTitle;
+
+    public Book(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+}
